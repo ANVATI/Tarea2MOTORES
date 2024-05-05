fadc84e58ec2fb3507de819b467460ed12142717
Wilber Antonio Vasquez Tisza <antoniotisza@gmail.com>
2024-05-05 14:53:27 -0500

    Invulnerabilidad y retornar a las posición inicial (Pausa para irme a almorzar)

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -8,28 +8,33 @@ public class PlayerMovement : MonoBehaviour
     //public KeyCode up;
     //public KeyCode down;
     private Rigidbody2D myRB;
+    public Transform InitialPoint;
     [SerializeField]
     private float speed;
     private float limitSuperior;
     private float limitInferior;
     public int player_lives = 4;
     public int points;
+    Vector2 _movement;
+    private bool isInvulnerable = false;
+    private Collider2D myCollider;
     void Start()
     {
         SetMinMax();
         myRB = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
         //if (up == KeyCode.None) up = KeyCode.UpArrow;
         //if (down == KeyCode.None) down = KeyCode.DownArrow;
     }
 
     void Update()
     {
-        /*
-        if (Input.GetKey(up) && transform.position.y < limitSuperior)
+
+        if (_movement.y > 0 && transform.position.y < limitSuperior)
         {
             myRB.velocity = new Vector2(0f, speed);
         }
-        else if (Input.GetKey(down) && transform.position.y > limitInferior)
+        else if (_movement.y < 0 && transform.position.y > limitInferior)
         {
             myRB.velocity = new Vector2(0f, -speed);
         }
@@ -37,30 +42,13 @@ public class PlayerMovement : MonoBehaviour
         {
             myRB.velocity = Vector2.zero;
         }
-        */
+
     }
-    public void OnMovementUp(InputAction.CallbackContext context)
+    public void OnMovement(InputAction.CallbackContext context)
     {
-        if(context.performed && transform.position.y < limitSuperior)
-        {
-            myRB.velocity = new Vector2(0f, speed);
-        }
-        else if (context.canceled)
-        {
-            myRB.velocity = Vector2.zero;
-        }
+        _movement = context.ReadValue<Vector2>();   
     }
-    public void OnMovementDown(InputAction.CallbackContext context)
-    {
-        if (context.performed && transform.position.y > limitInferior)
-        {
-            myRB.velocity = new Vector2(0f, -speed);
-        }
-        else if (context.canceled)
-        {
-            myRB.velocity = Vector2.zero;
-        }
-    }
+
     void SetMinMax()
     {
         Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
@@ -76,7 +64,23 @@ public class PlayerMovement : MonoBehaviour
         }
         if (other.tag == "Obstacle")
         {
-            ObstacleGenerator.instance.ManagerObstacle(other.gameObject.GetComponent<ObstacleController>(), this);
+            if (!isInvulnerable)
+            {
+                ObstacleGenerator.instance.ManagerObstacle(other.gameObject.GetComponent<ObstacleController>(), this);
+                transform.position = InitialPoint.position;
+                StartCoroutine(Invulnerability());
+
+            }
         }
+    }
+    IEnumerator Invulnerability()
+    {
+        myCollider.enabled = false;
+        isInvulnerable = true;
+
+        yield return new WaitForSeconds(1f);
+
+        myCollider.enabled = true;
+        isInvulnerable = false;
     }
 }
